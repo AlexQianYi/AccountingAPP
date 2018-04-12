4aed70d85fccb861e555fa989b57be592c71b2f1
AlexQianYi <q563875735@gmail.com>
2018-04-12 19:05:21 -0400

    keyboard_event

--- AccountingApp/app/src/main/java/com/example/yiqian/accountingapp/AddRecordActivity.java
+++ AccountingApp/app/src/main/java/com/example/yiqian/accountingapp/AddRecordActivity.java
@@ -1,13 +1,74 @@
 package com.example.yiqian.accountingapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class AddRecordActivity extends AppCompatActivity {
+public class AddRecordActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static String TAG = "AddRecordActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_record);
+
+        findViewById(R.id.keyboard_one).setOnClickListener(this);
+        findViewById(R.id.keyboard_two).setOnClickListener(this);
+        findViewById(R.id.keyboard_three).setOnClickListener(this);
+        findViewById(R.id.keyboard_four).setOnClickListener(this);
+        findViewById(R.id.keyboard_five).setOnClickListener(this);
+        findViewById(R.id.keyboard_six).setOnClickListener(this);
+        findViewById(R.id.keyboard_seven).setOnClickListener(this);
+        findViewById(R.id.keyboard_eight).setOnClickListener(this);
+        findViewById(R.id.keyboard_nine).setOnClickListener(this);
+        findViewById(R.id.keyboard_zero).setOnClickListener(this);
+
+    }
+
+    private void handleDot(){
+        findViewById(R.id.keyboard_dot).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void handleDot(){
+        findViewById(R.id.keyboard_dot).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void handleDot(){
+        findViewById(R.id.keyboard_dot).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void handleDot(){
+        findViewById(R.id.keyboard_dot).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button button = (Button) v;
+        String input = button.getText().toString();
+
+        Log.d(TAG, input);
     }
 }
--- AccountingApp/app/src/main/java/com/example/yiqian/accountingapp/MainActivity.java
+++ AccountingApp/app/src/main/java/com/example/yiqian/accountingapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.yiqian.accountingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +24,15 @@ public class MainActivity extends AppCompatActivity {
         pagerAdapter.notifyDataSetChanged();
         viewPager.setAdapter(pagerAdapter);
         viewPager.setCurrentItem(pagerAdapter.getLastIndex());
+
+        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddRecordActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
