075d8612b233e0e9e70ab24e2c5dc0bca6489a5e
AlexQianYi <q563875735@gmail.com>
2018-04-11 23:55:27 -0400

    head

--- AccountingApp/app/src/main/java/com/example/yiqian/accountingapp/MainActivity.java
+++ AccountingApp/app/src/main/java/com/example/yiqian/accountingapp/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.yiqian.accountingapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.robinhood.ticker.TickerUtils;
+import com.robinhood.ticker.TickerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TickerView tickerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +17,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         RecordBean bean = new RecordBean();
+
+
+        tickerView = (TickerView) findViewById(R.id.ticker_view);
+        tickerView.setCharacterList(TickerUtils.getDefaultNumberList());
+        tickerView.setText("9000");
+
+        tickerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tickerView.setText("1234");
+            }
+        });
     }
 }
