c29b91f3c88a0ef350e1825626554842fdd177d2
AlexQianYi <q563875735@gmail.com>
2018-04-12 11:32:49 -0400

    fragment

---
+++ AccountingApp/app/src/main/java/com/example/yiqian/accountingapp/MainFragment.java
@@ -0,0 +1,39 @@
+package com.example.yiqian.accountingapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+/**
+ * Created by yiqian on 2018/4/12.
+ */
+
+public class MainFragment extends Fragment {
+
+    private TextView textView;
+    private ListView listView;
+
+    private String date = "";
+
+    private View rootView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_main,container, false);
+
+        return rootView;
+
+    }
+
+    private void initView(){
+        textView = (TextView) rootView.findViewById(R.id.day_text);
+        listView = (ListView) rootView.findViewById(R.id.listView);
+        textView.setText(date);
+    }
+}
